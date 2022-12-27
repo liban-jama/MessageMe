029c5d7fc0f19e6763bf634d45316af65f175a26
Liban Jama <libanjama45@gmail.com>
2022-12-27 14:13:14 -0800

    initial backend || Connected to MongoDB || Created DB Schema

---
+++ whatsappbackend/dbMessages.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const whatsAppSchema = mongoose.Schema({
+    message: String,
+    name: String,
+    timeStamp: String,
+    received: Boolean
+});
+
+const messagesModel = mongoose.model('messageContent', whatsAppSchema);
+
+export default messagesModel
---
+++ whatsappbackend/server.js
@@ -0,0 +1,37 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import mongoose from 'mongoose';
+import messagesModel from "./dbMessages.js";
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(express.json());
+
+mongoose.connect(process.env.CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+
+})
+.then(() => console.log("MongoDB connection established"))
+.catch((error) => console.error("MongoDB connection failed: ", error.message));
+
+app.get('/', (req, res) => res.status(200).send("Hello World")); 
+
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
+
+  messagesModel.create(dbMessage, (err, data) => {
+    if (err) {
+      console.error("Error creating message: ", err.message);
+      res.status(500).send(err)
+    } else {
+      res.status(201).send(data)
+    }
+  });
+});
+
+
+app.listen(port, () => console.log(`We are listening at local host:${port}`));
